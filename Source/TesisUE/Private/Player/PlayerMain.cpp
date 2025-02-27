8a2ce5c90d3e86b016074647dd435b550d0f1658
ffeIipe <felipealderete03@gmail.com>
2025-02-26 22:05:01 -0300

    movement added in cpp

--- Source/TesisUE/Private/Player/PlayerMain.cpp
+++ Source/TesisUE/Private/Player/PlayerMain.cpp
@@ -2,33 +2,73 @@
 
 
 #include "Player/PlayerMain.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
 
-// Sets default values
 APlayerMain::APlayerMain()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
-// Called when the game starts or when spawned
 void APlayerMain::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (APlayerController* PlayerController = CastChecked<APlayerController>(GetController()))
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+			Subsystem->AddMappingContext(CharacterContext, 0);
 }
 
-// Called every frame
+void APlayerMain::Move(const FInputActionValue& Value)
+{
+	const FVector2D MoveVector = Value.Get<FVector2D>();
+
+	const FRotator ControlRotation = GetControlRotation();
+	const FRotator YawRotation(0.f, ControlRotation.Yaw, 0.f);
+
+	const FVector DirectionForward = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector DirectionSideward = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(DirectionForward, MoveVector.Y);
+	AddMovementInput(DirectionSideward, MoveVector.X);
+}
+
+void APlayerMain::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookingVector = Value.Get<FVector2D>();
+
+	AddControllerPitchInput(LookingVector.Y);
+	AddControllerYawInput(LookingVector.X);
+}
+
 void APlayerMain::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void APlayerMain::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerMain::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerMain::Look);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &APlayerMain::Jump);
+		//EnhancedInputComponent->BindAction(InteractAction, ETriggerEvent::Triggered, this, &APlayerMain::Interact);
+		//EnhancedInputComponent->BindAction(AttackAction, ETriggerEvent::Triggered, this, &APlayerMain::Attack);
+	}
 }
 
--- Source/TesisUE/Public/Player/PlayerMain.h
+++ Source/TesisUE/Public/Player/PlayerMain.h
@@ -6,24 +6,52 @@
 #include "GameFramework/Character.h"
 #include "PlayerMain.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 UCLASS()
 class TESISUE_API APlayerMain : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
+
 	APlayerMain();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputMappingContext* CharacterContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* MoveAction;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* LookAction;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* JumpAction;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* DodgeAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* InteractAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* AttackAction;
+
+private:	
+	
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	//void Interact(const FInputActionValue& Value);
+	//void Attack(const FInputActionValue& Value);
 };
