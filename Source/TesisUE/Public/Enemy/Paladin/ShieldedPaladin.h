d523a349fbebef7e9a1c9315316ee6b32de2e0dd
ffeIipe <felipealderete03@gmail.com>
2025-03-26 16:38:37 -0300

    working on shielded paladin

---
+++ Source/TesisUE/Public/Enemy/Paladin/ShieldedPaladin.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/Paladin.h"
+#include "ShieldedPaladin.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESISUE_API AShieldedPaladin : public APaladin
+{
+	GENERATED_BODY()
+	
+};
