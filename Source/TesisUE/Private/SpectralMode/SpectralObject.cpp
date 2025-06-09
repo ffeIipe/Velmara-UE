4040bc1e0ce177eb5fb3c1bee38a96490e8458fe
ffeIipe <felipealderete03@gmail.com>
2025-06-09 15:27:23 -0300

    fixes

--- Source/TesisUE/Private/SpectralMode/SpectralObject.cpp
+++ Source/TesisUE/Private/SpectralMode/SpectralObject.cpp
@@ -1,12 +1,15 @@
 #include "SpectralMode/SpectralObject.h"
 #include "Components/StaticMeshComponent.h"
 #include "Components/BoxComponent.h"
+#include "Components/SpectralObjectComponent.h"
 
 ASpectralObject::ASpectralObject()
 {
     PrimaryActorTick.bCanEverTick = false;
 
     ItemMesh->SetCollisionObjectType(ECC_GameTraceChannel1);
+
+    SpectralObjectComponent = CreateDefaultSubobject<USpectralObjectComponent>(TEXT("SpectralObjectComponent"));
 }
 
 void ASpectralObject::BeginPlay()
